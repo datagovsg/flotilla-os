3ef92050495e106cc8edd4f71454bda0b4826dad
I <patricksun2011@gmail.com>
2018-01-22 21:02:25 -0800

    Test popmotion w/o popmotion-react

--- src/components/LandingSplash.js
+++ src/components/LandingSplash.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { Component } from "react"
 import Link from "gatsby-link"
 import styled from "styled-components"
+import { tween, styler, easing, stagger, timeline } from "popmotion"
 import styles from "../constants/styles"
 import colors from "../constants/colors"
 
@@ -15,15 +16,21 @@ const LandingSplashInnerDiv = styled.div`
   ${styles.mixins.flex("column", "nowrap", "center", "flex-start", true, styles.shared.spacing * 2)}
   height: 600px;
 `
-const LandingSplashTagline = styled.div`
+const LandingSplashTagline = styled.div.attrs({
+  id: "landingSplashTagline",
+})`
   font-size: 20px;
 `
-const LandingSplashLargeText = styled.div`
+const LandingSplashLargeText = styled.div.attrs({
+  id: "landingSplashLargeText",
+})`
   font-size: 52px;
   line-height: 1.125;
   max-width: 600px;
 `
-const LandingSplashButtonGroup = styled.div`
+const LandingSplashButtonGroup = styled.div.attrs({
+  id: "landingSplashButtonGroup",
+})`
   ${styles.mixins.flex("row", "nowrap", "flex-start", "center", true)}
 `
 const LandingSplashButton = styled(Link)`
@@ -38,21 +45,55 @@ const LandingSplashButton = styled(Link)`
   line-height: 1;
 `
 
-const LandingSplash = () => (
-  <LandingSplashContainerDiv>
-    <LandingSplashInnerDiv>
-      <LandingSplashTagline>
-        You don't need data engineers. You need Flotilla.
-      </LandingSplashTagline>
-      <LandingSplashLargeText>
-        Some words about running containers and data science and stuff.
-      </LandingSplashLargeText>
-      <LandingSplashButtonGroup>
-        <LandingSplashButton to="/docs/quickstart">Documentation</LandingSplashButton>
-        <LandingSplashButton to="/">Github</LandingSplashButton>
-      </LandingSplashButtonGroup>
-    </LandingSplashInnerDiv>
-  </LandingSplashContainerDiv>
-)
+// Animations
+
+class LandingSplash extends Component {
+  componentDidMount() {
+    const landingSplashTagline = styler(document.querySelector("#landingSplashTagline"))
+    const landingSplashLargeText = styler(document.querySelector("#landingSplashLargeText"))
+    const landingSplashButtonGroup = styler(document.querySelector("#landingSplashButtonGroup"))
+
+    const staggerDuration = -300
+    const motionDuration = 500
+    const initialYPos = 80
+
+    const sharedMotion = {
+      from: { opacity: 0, y: initialYPos },
+      to: { opacity: 1, y: 0 },
+      ease: easing.ease,
+      duration: motionDuration,
+    }
+
+    timeline([
+      { ...sharedMotion, track: "tagline", },
+      `${staggerDuration}`,
+      { ...sharedMotion, track: "largeText", },
+      `${staggerDuration}`,
+      { ...sharedMotion, track: "buttonGroup", },
+    ]).start(({ tagline, largeText, buttonGroup }) => {
+      landingSplashTagline.set(tagline)
+      landingSplashLargeText.set(largeText)
+      landingSplashButtonGroup.set(buttonGroup)
+    })
+  }
+  render() {
+    return (
+      <LandingSplashContainerDiv>
+        <LandingSplashInnerDiv>
+          <LandingSplashTagline>
+            You don't need data engineers. You need Flotilla.
+          </LandingSplashTagline>
+          <LandingSplashLargeText>
+            Some words about running containers and data science and stuff.
+          </LandingSplashLargeText>
+          <LandingSplashButtonGroup>
+            <LandingSplashButton to="/docs/quickstart">Documentation</LandingSplashButton>
+            <LandingSplashButton to="/">Github</LandingSplashButton>
+          </LandingSplashButtonGroup>
+        </LandingSplashInnerDiv>
+      </LandingSplashContainerDiv>
+    )
+  }
+}
 
 export default LandingSplash
