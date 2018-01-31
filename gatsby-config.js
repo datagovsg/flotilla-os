9d8a5bbe5260f83e1045bd5a833986fe32269bab
I <patricksun2011@gmail.com>
2018-01-30 19:55:08 -0800

    Install gatsby-plugin-favicon package

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,5 +21,23 @@ module.exports = {
         sizeByPixelDensity: false,
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/assets/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
   ],
 };
