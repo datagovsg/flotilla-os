417a812127497bd24f01107d4a6a5e9a78e01561
I <patricksun2011@gmail.com>
2018-01-30 19:56:40 -0800

    Add favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from "react"
 import Link from "gatsby-link"
 import styled from "styled-components"
+import { BookOpen, Github } from "react-feather"
+import Logo from "../assets/favicon.png"
 import styles from "../constants/styles"
 import colors from "../constants/colors"
 
@@ -14,6 +16,7 @@ const HeaderContainerDiv = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: ${styles.zIndex.header},
   a {
     color: ${styles.shared.color} !important;
     border-bottom: 2px solid transparent;
@@ -31,16 +34,38 @@ const HeaderInnerDiv = styled.div`
 const HeaderLinksDiv = styled.div`
   ${styles.mixins.flex("row", "nowrap", "flex-start", "center", true, styles.shared.spacing * 1.5)}
 `
+const HeaderLinkStyles = `${styles.mixins.flex("row", "nowrap", "flex-start", "center", true)}`
+const HeaderRouterLink = styled(Link)`
+  ${HeaderLinkStyles}
+`
+const HeaderLink = styled.a`
+  ${HeaderLinkStyles}
+`
+const LogoImg = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 6px;
+  margin: 0;
+  margin-right: ${styles.shared.spacing}px;
+  transform: translateY(-3px);
+`
 
 const Header = () => (
   <HeaderContainerDiv>
     <HeaderInnerDiv>
-      <div>
-        <Link to="/">Flotilla</Link>
-      </div>
       <HeaderLinksDiv>
-        <Link activeClassName="active" to="/docs/philosophy">Documentation</Link>
-        <a target="_blank" href="https://github.com/stitchfix/flotilla-os">Github</a>
+        <LogoImg src={Logo} alt="stitchfix-logo" />
+        <Link to="/">Flotilla</Link>
+      </HeaderLinksDiv>
+      <HeaderLinksDiv>
+        <HeaderRouterLink activeClassName="active" to="/docs/philosophy">
+          <BookOpen />
+          Documentation
+        </HeaderRouterLink>
+        <HeaderLink target="_blank" href="https://github.com/stitchfix/flotilla-os">
+          <Github />
+          Github
+        </HeaderLink>
       </HeaderLinksDiv>
     </HeaderInnerDiv>
   </HeaderContainerDiv>
