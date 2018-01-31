5e4e4290578faa58da8fbfa6e39178a7e86312be
I <patricksun2011@gmail.com>
2018-01-30 19:56:24 -0800

    Add <LinkButton> component and remove image

--- src/components/LandingSplash.js
+++ src/components/LandingSplash.js
@@ -5,6 +5,7 @@ import { tween, styler, easing, stagger, timeline } from "popmotion"
 import styles from "../constants/styles"
 import colors from "../constants/colors"
 import SplashImg from "../assets/running-run-no-logs.png"
+import LinkButton from "./LinkButton"
 import LandingPageSection from "./LandingPageSection"
 import withScrollAnimation from "./withScrollAnimation"
 
@@ -14,15 +15,6 @@ const LandingSplashContent = styled.div`
   height: 100%;
   width: 100%;
   position: relative;
-`
-
-const LandingSplashImage = styled.img.attrs({
-  id: "landingSplashImage"
-})`
-  position: absolute;
-  top: -84px;
-  right: -450px;
-  transform: scale(0.7);
 `
 
 const LandingSplashTagline = styled.div.attrs({
@@ -42,23 +34,11 @@ const LandingSplashButtonGroup = styled.div.attrs({
 })`
   ${styles.mixins.flex("row", "nowrap", "flex-start", "center", true)}
 `
-const LandingSplashButton = styled(Link)`
-  ${styles.mixins.flex("row", "nowrap", "center", "center", true)}
-  outline: none;
-  background: transparent;
-  -webkit-appearance: none;
-  border: none;
-  background: ${colors.blue_0};
-  color: white !important;
-  padding: ${styles.shared.spacing}px ${styles.shared.spacing * 2}px;
-  line-height: 1;
-`
 
 const animate = () => {
   const landingSplashTagline = styler(document.querySelector("#landingSplashTagline"))
   const landingSplashLargeText = styler(document.querySelector("#landingSplashLargeText"))
   const landingSplashButtonGroup = styler(document.querySelector("#landingSplashButtonGroup"))
-  const landingSplashImage = styler(document.querySelector("#landingSplashImage"))
 
   const staggerDuration = -350
   const motionDuration = 500
@@ -78,18 +58,10 @@ const animate = () => {
     `${staggerDuration}`,
     { ...sharedMotion, track: "buttonGroup", },
     `${-500}`,
-    {
-      from: { opacity: 0, x: 200, scale: 0.7 },
-      to: { opacity: 1, x: 0, scale: 0.7 },
-      ease: easing.easeOut,
-      duration: 600,
-      track: "image"
-    }
-  ]).start(({ tagline, largeText, buttonGroup, image }) => {
+  ]).start(({ tagline, largeText, buttonGroup }) => {
     landingSplashTagline.set(tagline)
     landingSplashLargeText.set(largeText)
     landingSplashButtonGroup.set(buttonGroup)
-    landingSplashImage.set(image)
   })
 }
 
@@ -109,9 +81,8 @@ class LandingSplash extends Component {
             Some words about running containers and data science and stuff.
           </LandingSplashLargeText>
           <LandingSplashButtonGroup>
-            <LandingSplashButton to="/docs/quickstart">Get Started</LandingSplashButton>
+            <LinkButton to="/docs/quickstart">Get Started</LinkButton>
           </LandingSplashButtonGroup>
-          <LandingSplashImage src={SplashImg} alt="flotilla-ui-image" />
         </LandingSplashContent>
       </LandingPageSection>
     )
