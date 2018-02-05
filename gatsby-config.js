0f86fc349b9eca58793fffc0170ee2dc4c9422ba
I <patricksun2011@gmail.com>
2018-02-04 17:54:20 -0800

    Add gatsby-plugin-google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,12 @@ module.exports = {
           windows: false
         }
       }
-    }
+    },
+    //{
+    //  resolve: "gatsby-plugin-google-analytics",
+    //  options: {
+    //    trackingId: "FILL_ME_IN",
+    //  }
+    //}
   ],
 };
