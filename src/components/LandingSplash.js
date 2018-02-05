f36bc9f79f286ac0fb79ad3c5a0524a77e14de26
I <patricksun2011@gmail.com>
2018-02-05 10:57:04 -0800

    Add prettier

--- src/components/LandingSplash.js
+++ src/components/LandingSplash.js
@@ -4,39 +4,38 @@ import LandingPageSection from "./LandingPageSection"
 import withScrollAnimation from "./withScrollAnimation"
 import splashMotion from "../motion/splash"
 
-const defaultText = "Flotilla is a self-service framework that dramatically simplifies the process of defining and executing containerized jobs for data science."
-const textOptions = [
-  {
-    header: "You don't need data engineers 😉",
-    text: "You need Flotilla, a self-service framework that dramatically simplifies the process of defining and executing containerized jobs for data science.",
-  },
-  {
-    header: "Self-service data science.",
-    text: defaultText,
-  },
-  {
-    header: "Engineers shouldn't write ETL 😉",
-    text: defaultText,
-  }
-]
-
 class LandingSplash extends Component {
   render() {
-    const index = 0
-    const { header, text } = textOptions[index]
     return (
       <LandingPageSection id="splash" innerRef={this.props.innerRef}>
         <div className="fl-landing-section-content fl-splash">
-          <h1 className="fl-splash-text fl-splash-header" id="splashHeader">{header}</h1>
-          <p className="fl-splash-text" id="splashParagraph">{text}</p>
+          <h1 className="fl-splash-text fl-splash-header" id="splashHeader">
+            asdf
+          </h1>
+          <p className="fl-splash-text" id="splashParagraph">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non vero
+            blanditiis laborum voluptatum, voluptatibus perspiciatis. Repellat
+            quibusdam deleniti similique quos ut cum ipsa adipisci animi ipsum.
+            Eligendi ex, id unde!
+          </p>
           <pre className="fl-splash-code" id="splashCode">
             <code>git clone git@github.com:stitchfix/flotilla-os.git</code>
             <code>docker-compose up --build -d</code>
           </pre>
           <div className="fl-splash-buttons" id="splashButtons">
-            <Link className="pl-button pl-intent-primary" to="/docs/introduction">Get Started</Link>
+            <Link
+              className="pl-button pl-intent-primary"
+              to="/docs/introduction"
+            >
+              Get Started
+            </Link>
           </div>
-          <img className="fl-splash-image" src="https://user-images.githubusercontent.com/166823/35580026-038ae348-059d-11e8-95e4-f0150400a1a8.png" alt="flotilla-ui-image" id="splashImage" />
+          <img
+            className="fl-splash-image"
+            src="https://user-images.githubusercontent.com/166823/35580026-038ae348-059d-11e8-95e4-f0150400a1a8.png"
+            alt="flotilla-ui-image"
+            id="splashImage"
+          />
         </div>
       </LandingPageSection>
     )
@@ -46,5 +45,5 @@ class LandingSplash extends Component {
 export default withScrollAnimation({
   elRef: "x",
   animate: splashMotion,
-  heightRenderedBeforeAnimationStarts: 100
+  heightRenderedBeforeAnimationStarts: 100,
 })(LandingSplash)
